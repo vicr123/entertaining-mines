b01a832ecd45d21465c75173bae2f7d9ba3a1846
Victor Tran <vicr12345@gmail.com>
2019-10-30 15:11:36 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 93.1% (95 of 102 strings)
    
    Translation: Entertaining Mines/Entertaining Mines Desktop
    Translate-URL: https://translate.vicr123.com/projects/entertaining-mines/entertaining-mines-desktop/vi/

--- translations/vi_VN.ts
+++ translations/vi_VN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../game/congratulation.ui" line="79"/>
         <source>Congratulations!</source>
-        <translation type="unfinished"></translation>
+        <translation>Chúc mừng!</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="89"/>
@@ -16,38 +16,38 @@
     <message>
         <location filename="../game/congratulation.ui" line="106"/>
         <source>Play Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Chơi lại</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="113"/>
         <source>Play a new board with the same parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Chơi bảng mới với các tham số giống nhau</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="120"/>
         <source>Review</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem lại</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="127"/>
         <source>See the board</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem bảng</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="150"/>
         <location filename="../game/congratulation.cpp" line="28"/>
         <source>Main Menu</source>
-        <translation type="unfinished">Trang chủ</translation>
+        <translation>Trang chủ</translation>
     </message>
     <message>
         <location filename="../game/congratulation.ui" line="157"/>
         <source>Return to the Main Menu</source>
-        <translation type="unfinished">Trở lại trang chủ</translation>
+        <translation>Trở lại trang chủ</translation>
     </message>
     <message>
         <location filename="../game/congratulation.cpp" line="27"/>
         <source>Select</source>
-        <translation type="unfinished">Chọn</translation>
+        <translation>Chọn</translation>
     </message>
 </context>
 <context>
@@ -55,42 +55,42 @@
     <message>
         <location filename="../information/creditsscreen.ui" line="71"/>
         <source>Credits</source>
-        <translation type="unfinished"></translation>
+        <translation>Tín dụng</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="158"/>
         <source>Entertaining Mines</source>
-        <translation type="unfinished">Mìn giải trí</translation>
+        <translation>Mìn giải trí</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="180"/>
         <source>DEVELOPED BY</source>
-        <translation type="unfinished"></translation>
+        <translation>PHÁT TRIỂN BỞI</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="233"/>
         <source>MUSIC BY</source>
-        <translation type="unfinished"></translation>
+        <translation>NHẠC BỞI</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="264"/>
         <source>Background Music - &quot;Crypto&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc nền - &quot;Crypto&quot;</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="277"/>
         <source>Defeat Music - &quot;Piano Between&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc thua - &quot;Piano Between&quot;</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="290"/>
         <source>Victory Music - &quot;Disco Sting&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc thắng - &quot;Disco Sting&quot;</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="303"/>
         <source>Music is licensed under Creative Commons: BY 4.0</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc được cấp phép theo Creative Commons: BY 4.0</translation>
     </message>
     <message>
         <location filename="../information/creditsscreen.ui" line="332"/>
@@ -100,7 +100,7 @@
     <message>
         <location filename="../information/creditsscreen.cpp" line="44"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Lại</translation>
     </message>
 </context>
 <context>
@@ -121,38 +121,38 @@
     <message>
         <location filename="../game/gameover.ui" line="96"/>
         <source>Play Again</source>
-        <translation type="unfinished"></translation>
+        <translation>Chơi lại</translation>
     </message>
     <message>
         <location filename="../game/gameover.ui" line="103"/>
         <source>Play a new board with the same parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Chơi bảng mới với các tham số giống nhau</translation>
     </message>
     <message>
         <location filename="../game/gameover.ui" line="110"/>
         <source>Review</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem lại</translation>
     </message>
     <message>
         <location filename="../game/gameover.ui" line="117"/>
         <source>See the board</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem bảng</translation>
     </message>
     <message>
         <location filename="../game/gameover.ui" line="140"/>
         <location filename="../game/gameover.cpp" line="28"/>
         <source>Main Menu</source>
-        <translation type="unfinished">Trang chủ</translation>
+        <translation>Trang chủ</translation>
     </message>
     <message>
         <location filename="../game/gameover.ui" line="147"/>
         <source>Return to the Main Menu</source>
-        <translation type="unfinished">Trở lại trang chủ</translation>
+        <translation>Trở lại trang chủ</translation>
     </message>
     <message>
         <location filename="../game/gameover.cpp" line="27"/>
         <source>Select</source>
-        <translation type="unfinished">Chọn</translation>
+        <translation>Chọn</translation>
     </message>
 </context>
 <context>
@@ -182,12 +182,12 @@
     <message>
         <location filename="../screens/gamescreen.cpp" line="218"/>
         <source>In Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chơi</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="442"/>
         <source>Mark</source>
-        <translation type="unfinished"></translation>
+        <translation>Đánh dấu</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="445"/>
@@ -202,58 +202,52 @@
     <message>
         <location filename="../screens/gamescreen.cpp" line="306"/>
         <source>Your time was not recorded because loading a save invalidates the timer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Thời gian chơi của bạn không có bị ghi tại vì khi bạn mở bản lưu, bộ hẹn giờ sẽ bị vô hiệu.</translation>
     </message>
     <message numerus="yes">
         <location filename="../screens/gamescreen.cpp" line="219"/>
         <source>%1×%2 board with %n mines</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Bảng %1×%2 với %n só mìn</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../screens/gamescreen.cpp" line="368"/>
         <source>You completed a %1×%2 board with %n mines in %4. Divine!</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Bạn đã hoàn thành một bảng %1×%2 với %n só mìn trong %4. Làm tốt lắm!</numerusform>
+        </translation></message>
     <message numerus="yes">
         <location filename="../screens/gamescreen.cpp" line="373"/>
         <source>You completed a %1×%2 board with %n mines.</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>Bạn đã hoàn thành một bảng %1×%2 với %n só mìn.</numerusform>
+        </translation></message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="480"/>
         <source>Your time was not recorded because pausing the game invalidates the timer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Thời gian chơi của bạn không có bị ghi tại vì khi bạn tạm dừng phiên chơi, bộ hẹn giờ sẽ bị vô hiệu.</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="502"/>
         <source>%1 × %2 board</source>
-        <translation type="unfinished"></translation>
+        <translation>Bảng %1 × %2</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="503"/>
         <source>%1 mines</source>
-        <translation type="unfinished"></translation>
+        <translation>%n só mìn</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="504"/>
         <source>%1 flagged</source>
-        <translation type="unfinished"></translation>
+        <translation>%n đã đặt cờ</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="505"/>
         <source>%1 mines to go</source>
-        <translation type="unfinished"></translation>
+        <translation>Còn lại %n só mìn</translation>
     </message>
     <message>
         <location filename="../screens/gamescreen.cpp" line="506"/>
         <source>%1% cleared</source>
-        <translation type="unfinished"></translation>
+        <translation>%1% hoàn thành</translation>
     </message>
 </context>
 <context>
@@ -266,12 +260,12 @@
     <message>
         <location filename="../gamewindow.cpp" line="64"/>
         <source>Idle</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang nghỉ</translation>
     </message>
     <message>
         <location filename="../gamewindow.cpp" line="65"/>
         <source>Main Menu</source>
-        <translation type="unfinished">Trang chủ</translation>
+        <translation>Trang chủ</translation>
     </message>
 </context>
 <context>
@@ -284,12 +278,12 @@
     <message>
         <location filename="../information/helpscreen.ui" line="71"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Trợ giúp</translation>
     </message>
     <message>
         <location filename="../information/helpscreen.cpp" line="38"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Lại</translation>
     </message>
 </context>
 <context>
@@ -363,12 +357,12 @@
         <location filename="../screens/mainscreen.ui" line="254"/>
         <location filename="../screens/mainscreen.ui" line="330"/>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Thông tin</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="261"/>
         <source>Get help or view information about the game</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhận trợ giúp hoặc xem thông tin về trò chơi</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="268"/>
@@ -383,32 +377,32 @@
     <message>
         <location filename="../screens/mainscreen.ui" line="347"/>
         <source>Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Trợ giúp</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="354"/>
         <source>Get information about how to play</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhận thông tin về cách chơi</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="361"/>
         <source>Credits</source>
-        <translation type="unfinished"></translation>
+        <translation>Tín dụng</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="368"/>
         <source>See who made the game</source>
-        <translation type="unfinished"></translation>
+        <translation>Xem ai làm trò chơi</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="391"/>
         <source>Main Menu</source>
-        <translation type="unfinished">Trang chủ</translation>
+        <translation>Trang chủ</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.ui" line="398"/>
         <source>Return to the Main Menu</source>
-        <translation type="unfinished">Trở lại trang chủ</translation>
+        <translation>Trở lại trang chủ</translation>
     </message>
     <message>
         <location filename="../screens/mainscreen.cpp" line="49"/>
@@ -457,22 +451,22 @@
     <message>
         <location filename="../screens/pausescreen.ui" line="110"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Lưu</translation>
     </message>
     <message>
         <location filename="../screens/pausescreen.ui" line="117"/>
         <source>Save this game so you can resume it later</source>
-        <translation type="unfinished"></translation>
+        <translation>Lưu phiên chơi này để chơi giờ khắc</translation>
     </message>
     <message>
         <location filename="../screens/pausescreen.ui" line="124"/>
         <source>New Game</source>
-        <translation type="unfinished"></translation>
+        <translation>Trò chơi mới</translation>
     </message>
     <message>
         <location filename="../screens/pausescreen.ui" line="131"/>
         <source>Abandon this game and start a new one with the same parameters</source>
-        <translation type="unfinished"></translation>
+        <translation>Từ bỏ phiên chơi này và bắt đầu phiên mới với tham số giống nhau</translation>
     </message>
     <message>
         <location filename="../screens/pausescreen.ui" line="154"/>
@@ -505,47 +499,47 @@
     <message>
         <location filename="../screens/settingsscreen.ui" line="71"/>
         <source>Settings</source>
-        <translation type="unfinished">Cài đặt</translation>
+        <translation>Cài đặt</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="50"/>
         <source>Audio</source>
-        <translation type="unfinished"></translation>
+        <translation>Âm thanh</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="40"/>
         <source>Background Music</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc nền</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="41"/>
         <source>Sound Effects</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhạc hiệu ứng</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="53"/>
         <source>Behaviour</source>
-        <translation type="unfinished"></translation>
+        <translation>Hành vi</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="54"/>
         <source>Marks</source>
-        <translation type="unfinished"></translation>
+        <translation>Dấu</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="55"/>
         <source>When this is enabled, right clicking a flag will change it into the marked state first.</source>
-        <translation type="unfinished"></translation>
+        <translation>Khi tùy này bật, nếu bạn nhấn phải trên một cờ, nó sẽ đổi sang chế độ đánh dấu trước.</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="77"/>
         <source>Select</source>
-        <translation type="unfinished">Chọn</translation>
+        <translation>Chọn</translation>
     </message>
     <message>
         <location filename="../screens/settingsscreen.cpp" line="78"/>
         <source>Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Lại</translation>
     </message>
 </context>
 <context>
